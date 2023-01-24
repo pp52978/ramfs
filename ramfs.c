7f327a529c2637bb9476c56cb8ddb17b778655ae
pp <631843571@qq.com>
2023-01-24 16:27:40 +0800

    1

--- ramfs.c
+++ ramfs.c
@@ -5,13 +5,20 @@
 #include <string.h>
 
 #define MAX_FD 66000 //同时存在的FD数量
+typedef struct package{
+    int num;
+    char *pack[100];//假定路径深度最多为100,可能不够
+}pkg;
 typedef struct node{
     enum type{ff,dd}type;
     void *content;
+    int size;
     char *name;
     bool Is_open;//当前是否被打开中
     int fd;//对应的fd
     int offset;
+    bool read;
+    bool write;
 }node;
 typedef struct child{
     int fd;
@@ -23,8 +30,76 @@ typedef struct FD{
     bool check;//是否被使用
 }FD;
 FD tree[MAX_FD];
+
+
+int check_valid(char *token){
+    int len = strlen(token);
+    if(len>32)
+        return -1;
+    else {
+        for(int i=0;i<len;i++)
+        {
+            if(!((token[i]>='A'&&token[i]<='Z')||(token[i]>='a'&&token[i]<='z')||(token[i]>='0'&&token[i]<='9')||(token[i]=='.')))
+                return -1;
+        }
+    }
+    return 0;
+}//检查文件名合法性
+struct package analyze(const char *pathname){
+    char tmp[1030];
+    strcpy(tmp,pathname);
+    pkg data;
+    data.num=0;
+    char *token;
+    token=strtok(tmp,"/");
+    while(token!=NULL)
+    {
+        if(check_valid(token) == -1){
+            data.num = 0;
+            break;
+        }
+        data.pack[data.num]=token;
+        data.num++;
+        token=strtok(NULL,"/");
+    }
+    data.num--;
+    return data;
+}
+int search(char * target,int start){
+    if(tree[start].child_field == NULL)
+        return -1;
+    else{
+        child *new_start = tree[start].child_field;
+        while (new_start != NULL){
+            if(strcmp(target,tree[new_start->fd].path->name)==0)
+                return new_start->fd;
+            else
+                new_start = new_start->next;
+        }
+        return -1;
+    }
+
+}//返回对象的fd值
+
 int ropen(const char *pathname, int flags) {
-  // TODO();
+    pkg a_name = analyze(pathname);
+    if(a_name.num==-1)
+        return -1;
+    int now = 0;//从根目录出发
+    if(pathname[0]!='/')
+        return -1;
+    for(int i = 0;i <= a_name.num;i++){
+        if(search(a_name.pack[i],now)==-1)
+            return -1;
+        else
+            now = search(a_name.pack[i],now);
+    }
+    if(tree[now].path->type == dd)
+        return now;
+    else {
+        int tplen = strlen(pathname)
+    }
+
 }
 
 int rclose(int fd) {
@@ -62,7 +137,10 @@ void init_ramfs() {
         tree[i].check = 0;
     }
     node *root = malloc(sizeof(node));
-    root -> type = dd;
+    root -> type = dd; //后续root可能需要进一步初始化
+    root ->fd = 0;
+    tree[0].path = root;//0号分给根目录
+
 
 
 
