1b7d3ba00af16eb3c3780242021c5eda7a92d17f
pp <631843571@qq.com>
2023-01-25 23:10:21 +0800

    2

--- ramfs.c
+++ ramfs.c
@@ -4,34 +4,33 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_FD 66000 //同时存在的FD数量
+#define MAX_FD 5000 //同时存在的FD数量
 typedef struct package{
     int num;
     char *pack[100];//假定路径深度最多为100,可能不够
 }pkg;
 typedef struct node{
     enum type{ff,dd}type;
+    struct node *first_child;//第一个孩子
+    struct node *next_brother;//下一个兄弟
     void *content;
     int size;
     char *name;
-    bool Is_open;//当前是否被打开中
-    int fd;//对应的fd
-    int offset;
-    bool read;
-    bool write;
 }node;
-typedef struct child{
-    int fd;
-    struct child *next;//下一个孩子
-}child;//孩子域
 typedef struct FD{
-    struct node * path;//对应文件位置
-    struct child * child_field;//孩子域
-    bool check;//是否被使用
+    node * path;//对应文件位置
+    int offset;
+    int flags;
+    bool Is_using;
 }FD;
 FD tree[MAX_FD];
+node root;
 
-
+int check_O_CREAT(int data){
+    if((data>>6)%2==1)
+        return 1;
+    return 0;
+}
 int check_valid(char *token){
     int len = strlen(token);
     if(len>32)
@@ -55,50 +54,94 @@ struct package analyze(const char *pathname){
     while(token!=NULL)
     {
         if(check_valid(token) == -1){
-            data.num = 0;
+            data.num = -1;
             break;
-        }
+        }//如果不合法退出
         data.pack[data.num]=token;
         data.num++;
         token=strtok(NULL,"/");
     }
-    data.num--;
     return data;
 }
-int search(char * target,int start){
-    if(tree[start].child_field == NULL)
-        return -1;
+node* search(char *target,node *start){
+    if(start->first_child == NULL)
+        return NULL;
     else{
-        child *new_start = tree[start].child_field;
+        node *new_start = start->first_child;
         while (new_start != NULL){
-            if(strcmp(target,tree[new_start->fd].path->name)==0)
-                return new_start->fd;
+            if(strcmp(target,new_start->name)==0)
+                return new_start;
             else
-                new_start = new_start->next;
+                new_start = new_start->next_brother;
         }
-        return -1;
+        return NULL;
     }
-
-}//返回对象的fd值
-
+}
+node* creat_file(node *start,const char *name){
+    node *newfile = malloc(sizeof(node));
+    newfile->first_child = NULL;
+    newfile->next_brother = NULL;
+    newfile->size = 0;
+    newfile->content = NULL;
+    newfile->type = ff;
+    newfile->name = name;
+    if(start->first_child == NULL){
+        start->first_child = newfile;
+    }
+    else {
+        node *newstart = start->first_child;
+        while (newstart->next_brother!=NULL){
+            newstart = newstart->next_brother;
+        }
+        newstart->next_brother = newfile;
+    }
+    return newfile;
+}
+int find_empty_fd(){
+    for(int i=0;i<MAX_FD;i++){
+        if(tree[i].Is_using == 0)
+            return i;
+    }
+}
 int ropen(const char *pathname, int flags) {
     pkg a_name = analyze(pathname);
     if(a_name.num==-1)
-        return -1;
-    int now = 0;//从根目录出发
+        return -1;//名称解析失败
+    node * now = &root;//从根目录出发
     if(pathname[0]!='/')
-        return -1;
-    for(int i = 0;i <= a_name.num;i++){
-        if(search(a_name.pack[i],now)==-1)
-            return -1;
+        return -1;//第一位不是'/'
+    for(int i = 0;i < a_name.num-1;i++){
+        if(search(a_name.pack[i],now)==NULL)
+            return -1;//找不到路径
         else
             now = search(a_name.pack[i],now);
+    }//此时的now是目标上级目录
+    if(search(a_name.pack[a_name.num-1],now)==NULL){    //找不到最终地址
+        if(check_O_CREAT(flags)==1){
+            int tmplen = strlen(pathname);
+            if(pathname[tmplen-1] == '/')
+                return -1;//文件名后是'/'
+            now = creat_file(now,a_name.pack[a_name.num-1]);       //创建
+        }
+        else{
+            return -1;
+        }
     }
-    if(tree[now].path->type == dd)
-        return now;
-    else {
-        int tplen = strlen(pathname)
+    else
+        now = search(a_name.pack[a_name.num-1],now);
+    if(now->type == ff && pathname[strlen(pathname)-1]=='/')//文件后有'/'
+        return -1;
+    if(now->type == dd){        //目录省略flag
+        int fd = find_empty_fd();
+        tree[fd].Is_using = 1;
+        tree[fd].path = now;
+        //tree[fd].flags=?;tree[fd].offset=?目录不设置
+        return fd;
     }
+    else{
+
+    }
+    
 
 }
 
@@ -132,19 +175,15 @@ int runlink(const char *pathname) {
 
 void init_ramfs() {
     for(int i=0;i<MAX_FD;i++) {
-        tree[i].child_field = NULL;
         tree[i].path = NULL;
-        tree[i].check = 0;
+        tree[i].Is_using = 0;
     }
-    node *root = malloc(sizeof(node));
-    root -> type = dd; //后续root可能需要进一步初始化
-    root ->fd = 0;
-    tree[0].path = root;//0号分给根目录
-
-
-
-
-
+    root.type = dd;
+    root.name = "/";
+    root.content = NULL;
+    root.size = -1;
+    root.first_child = NULL;
+    root.next_brother = NULL;
 }
 
 
