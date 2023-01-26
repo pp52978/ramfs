c4acde813479cea30e32fe4825eae6a5d39cbcb2
pp <631843571@qq.com>
2023-01-26 23:32:19 +0800

    compile

--- ramfs.c
+++ ramfs.c
@@ -60,6 +60,10 @@ struct package analyze(const char *pathname){
     strcpy(tmp,pathname);
     pkg data;
     data.num=0;
+    if(pathname[0]!='/'){
+        data.num=-1;
+        return data;
+    }
     char *token;
     token=strtok(tmp,"/");
     while(token!=NULL)
@@ -88,14 +92,16 @@ node* search(char *target,node *start){
         return NULL;
     }
 }
-node* creat_file(node *start,const char *name){
+node* creat_file(node *start,char *name){
     node *newfile = malloc(sizeof(node));
     newfile->first_child = NULL;
     newfile->next_brother = NULL;
     newfile->size = 0;
     newfile->content = NULL;
     newfile->type = ff;
-    newfile->name = name;
+    char *nname = malloc((strlen(name)+1)*sizeof(char));
+    strcpy(nname,name);
+    newfile->name = nname;//可能需要malloc
     if(start->first_child == NULL){
         start->first_child = newfile;
     }
@@ -107,6 +113,28 @@ node* creat_file(node *start,const char *name){
         newstart->next_brother = newfile;
     }
     return newfile;
+}
+node* creat_dir(node *start,char *name){
+    node *newdir = malloc(sizeof(node));
+    newdir->first_child = NULL;
+    newdir->next_brother = NULL;
+    newdir->size = 0;
+    newdir->content = NULL;
+    newdir->type = dd;
+    char *nname = malloc((strlen(name)+1)*sizeof(char));
+    strcpy(nname,name);
+    newdir->name = nname;//可能需要malloc
+    if(start->first_child == NULL){
+        start->first_child = newdir;
+    }
+    else {
+        node *newstart = start->first_child;
+        while (newstart->next_brother!=NULL){
+            newstart = newstart->next_brother;
+        }
+        newstart->next_brother = newdir;
+    }
+    return newdir;
 }
 int find_empty_fd(){
     for(int i=0;i<MAX_FD;i++){
@@ -120,8 +148,7 @@ int ropen(const char *pathname, int flags) {
     if(a_name.num==-1)
         return -1;//名称解析失败
     node * now = &root;//从根目录出发
-    if(pathname[0]!='/')
-        return -1;//第一位不是'/'
+
     for(int i = 0;i < a_name.num-1;i++){
         if(search(a_name.pack[i],now)==NULL)
             return -1;//找不到路径
@@ -181,11 +208,17 @@ int ropen(const char *pathname, int flags) {
 }
 
 int rclose(int fd) {
-  // TODO();
+    if(tree[fd].Is_using == 0)
+        return -1;
+    tree[fd].path = NULL;
+    tree[fd].Is_using = 0;
+    return 0;
 }
 
 ssize_t rwrite(int fd, const void *buf, size_t count) {
-  // TODO();
+    if(tree[fd].Is_using == 0||tree[fd].Can_write==0||tree[fd].path->type==dd)
+        return -1;
+
 }
 
 ssize_t rread(int fd, void *buf, size_t count) {
@@ -197,15 +230,93 @@ off_t rseek(int fd, off_t offset, int whence) {
 }
 
 int rmkdir(const char *pathname) {
+    pkg a_name = analyze(pathname);
+    if(a_name.num==-1||a_name.num==0)
+        return -1;
+    node *start = &root;
+    for(int i = 0;i < a_name.num-1;i++){
+        if(search(a_name.pack[i],start)==NULL)
+            return -1;//找不到路径
+        else
+            start = search(a_name.pack[i],start);
+    }//此时的start是目标上级目录
+    if (search(a_name.pack[a_name.num-1],start)!=NULL)
+        return -1;//已经存在
+    else{
+        creat_dir(start,a_name.pack[a_name.num-1]);
+        return 0;
+    }
 
 }
 
 int rrmdir(const char *pathname) {
   // TODO();
+    pkg a_name = analyze(pathname);
+    if(a_name.num==-1||a_name.num==0)
+        return -1;
+    node *start = &root;
+    for(int i = 0;i < a_name.num-1;i++){
+        if(search(a_name.pack[i],start)==NULL)
+            return -1;//找不到路径
+        else
+            start = search(a_name.pack[i],start);
+    }//此时的start是目标上级目录
+    if (search(a_name.pack[a_name.num-1],start)==NULL)
+        return -1;//不存在
+    node *father = start;
+    start = search(a_name.pack[a_name.num-1],start);//start为要删除的目录
+    if(start->type == ff||start->first_child != NULL)
+        return -1;//是文件或者不是空目录
+
+    if(father->first_child == start){
+        father->first_child = start->next_brother;
+    }
+    else {
+        node *newstart = father->first_child;
+        while(newstart->next_brother != start){
+            newstart = newstart->next_brother;
+        }
+        newstart->next_brother = start->next_brother;
+    }//维护链表
+
+    free(start->name);
+    free(start);
 }
 
 int runlink(const char *pathname) {
-  // TODO();
+  // TODO()检查末尾
+    pkg a_name = analyze(pathname);
+    if(a_name.num==-1||a_name.num==0||pathname[strlen(pathname)-1]=='/')
+        return -1;
+    node *start = &root;
+    for(int i = 0;i < a_name.num-1;i++){
+        if(search(a_name.pack[i],start)==NULL)
+            return -1;//找不到路径
+        else
+            start = search(a_name.pack[i],start);
+    }//此时的start是目标上级目录
+    if (search(a_name.pack[a_name.num-1],start)==NULL)
+        return -1;//不存在
+    node *father = start;
+    start = search(a_name.pack[a_name.num-1],start);//start为要删除的文件
+    if(start->type == dd)
+        return -1;//是目录
+
+    if(father->first_child == start){
+        father->first_child = start->next_brother;
+    }
+    else {
+        node *newstart = father->first_child;
+        while(newstart->next_brother != start){
+            newstart = newstart->next_brother;
+        }
+        newstart->next_brother = start->next_brother;
+    }//维护链表
+
+    free(start->content);
+    free(start->name);
+    free(start);
+
 }
 
 void init_ramfs() {
