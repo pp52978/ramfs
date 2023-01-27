b351f5985690b95b433519afb3239cabe6843732
pp <631843571@qq.com>
2023-01-27 17:07:27 +0800

    compile

--- main.c
+++ main.c
@@ -1,28 +1,67 @@
-/* our main.c */
-#include "ramfs.h"
-#include <assert.h>
-#include <string.h>
-int main() {
-    init_ramfs();     // 你的初始化操作
-    assert(rmkdir("/dir") == 0);   // 应当成功
-    assert(rmkdir("//dir") == -1);  // 应当给出 error，因为目录已存在
-    assert(rmkdir("/a/b") == -1);  // 应当给出 error，因为父目录不存在
-    int fd;
-    assert((fd = ropen("//dir///////1.txt", O_CREAT | O_RDWR)) > 0);  // 创建文件应当
-    //成功
-    assert(rwrite(fd, "hello", 5) == 5);  // 应当完整地写入
-    assert(rseek(fd, 0, SEEK_CUR) == 5);  // 当前 fd 的偏移量应该为 5
-    assert(rseek(fd, 0, SEEK_SET) == 0);  // 应当成功将 fd 的偏移量复位到文件开头
-    char buf[7];
-    assert(rread(fd, buf, 7) == 5);   // 只能读到 5 字节，因为文件只有 5 字节
-    assert(memcmp(buf, "hello", 5) == 0);   // rread 应当确实读到 "hello" 5 个字节
-    assert(rseek(fd, 3, SEEK_END) == 8);  // 文件大小为 5，向后 3 字节则是在第 8 字节
-    assert(rwrite(fd, "world", 5) == 5);  // 再写 5 字节
-    assert(rseek(fd, 5, SEEK_SET) == 5);  // 将偏移量重设到 5 字节
-    assert(rread(fd, buf, 8) == 8);   // 在第 8 字节后写入了 5 字节，文件大小 13 字节；那
-    //么从第 5 字节后应当能成功读到 8 字节
-    assert(memcmp(buf, "\0\0\0world", 8) == 0); // 3 字节的空隙应当默认填 0
-    assert(rclose(fd) == 0); // 关闭打开的文件应当成功
-    assert(rclose(fd + 1) == -1); //关闭未打开的文件应当失败
+int notin(int fd, int *fds, int n) {
+    for (int i = 0; i < n; i++) {
+        if (fds[i] == fd) return 0;
+    }
+    return 1;
+}
+int genfd(int *fds, int n) {
+    for (int i = 0; i < 4096; i++) {
+        if (notin(i, fds, n))
+            return i;
+    }
+    return -1;
+}
+int test2() {
+    init_ramfs();
+    int fd[10];
+    int buf[10];
+    assert(ropen("/abc==d", O_CREAT) == -1);
+    assert((fd[0] = ropen("/0", O_RDONLY)) == -1);
+    assert((fd[0] = ropen("/0", O_CREAT | O_WRONLY)) >= 0);
+    assert((fd[1] = ropen("/1", O_CREAT | O_WRONLY)) >= 0);
+    assert((fd[2] = ropen("/2", O_CREAT | O_WRONLY)) >= 0);
+    assert((fd[3] = ropen("/3", O_CREAT | O_WRONLY)) >= 0);
+    assert(rread(fd[0], buf, 1) == -1);
+    assert(rread(fd[1], buf, 1) == -1);
+    assert(rread(fd[2], buf, 1) == -1);
+    assert(rread(fd[3], buf, 1) == -1);
+    for (int i = 0; i < 100; i++) {
+        assert(rwrite(fd[0], "\0\0\0\0\0", 5) == 5);
+        assert(rwrite(fd[1], "hello", 5) == 5);
+        assert(rwrite(fd[2], "world", 5) == 5);
+        assert(rwrite(fd[3], "\x001\x002\x003\x0fe\x0ff", 5) == 5);
+    }
+    assert(rclose(fd[0]) == 0);
+    assert(rclose(fd[1]) == 0);
+    assert(rclose(fd[2]) == 0);
+    assert(rclose(fd[3]) == 0);
+    assert(rclose(genfd(fd, 4)) == -1);
+    assert((fd[0] = ropen("/0", O_CREAT | O_RDONLY)) >= 0);
+    assert((fd[1] = ropen("/1", O_CREAT | O_RDONLY)) >= 0);
+    assert((fd[2] = ropen("/2", O_CREAT | O_RDONLY)) >= 0);
+    assert((fd[3] = ropen("/3", O_CREAT | O_RDONLY)) >= 0);
+    assert(rwrite(fd[0], buf, 1) == -1);
+    assert(rwrite(fd[1], buf, 1) == -1);
+    assert(rwrite(fd[2], buf, 1) == -1);
+    assert(rwrite(fd[3], buf, 1) == -1);
+    for (int i = 0; i < 50; i++) {
+        assert(rread(fd[0], buf, 10) == 10);
+        assert(memcmp(buf, "\0\0\0\0\0\0\0\0\0\0", 10) == 0);
+        assert(rread(fd[1], buf, 10) == 10);
+        assert(memcmp(buf, "hellohello", 10) == 0);
+        assert(rread(fd[2], buf, 10) == 10);
+        assert(memcmp(buf, "worldworld", 10) == 0);
+        assert(rread(fd[3], buf, 10) == 10);
+        assert(memcmp(buf, "\x001\x002\x003\x0fe\x0ff\x001\x002\x003\x0fe\x0ff", 10)
+               == 0);
+    }
+    assert(rread(fd[0], buf, 10) == 0);
+    assert(rread(fd[1], buf, 10) == 0);
+    assert(rread(fd[2], buf, 10) == 0);
+    assert(rread(fd[3], buf, 10) == 0);
+    assert(rclose(fd[0]) == 0);
+    assert(rclose(fd[1]) == 0);
+    assert(rclose(fd[2]) == 0);
+    assert(rclose(fd[3]) == 0);
     return 0;
 }
